019132c2cba8a1ededdb7d5c099cc22b8ab1a050
Syafiq2112 <azman.syafiq01@gmail.com>
2024-03-09 22:42:13 +0800

    console log tutorials

--- IntroductionToFrontEnd/JavascriptCourse/test.js
+++ IntroductionToFrontEnd/JavascriptCourse/test.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+// 9/3/2024 writing your first Javascript code (not really)
+
+/*
+This is made by: Syafiq Bin Shamsul Azman
+Coursera Progamming with Javascript Course
+Start Date: 9/3/2024
+*/
+
+console.log('Hello World');
+console.log("%cHello, World", "color: blue; font-size:40px");
+console.log("Hello " + "there, " + "World");
