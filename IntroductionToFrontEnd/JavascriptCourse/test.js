ca967d618bc3c731bfd8750e75540477ee396fa9
Syafiq2112 <azman.syafiq01@gmail.com>
2024-03-09 23:23:15 +0800

    Update test.js

--- IntroductionToFrontEnd/JavascriptCourse/test.js
+++ IntroductionToFrontEnd/JavascriptCourse/test.js
@@ -8,4 +8,4 @@ Start Date: 9/3/2024
 
 console.log('Hello World');
 console.log("%cHello, World", "color: blue; font-size:40px");
-console.log("Hello " + "there, " + "World");
+console.log("Hello " + "there, " + "World" + 7);
