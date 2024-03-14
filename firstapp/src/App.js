e50949de9fbf2f942de662c9dd212e8b5d134362
Syafiq2112 <azman.syafiq01@gmail.com>
2024-03-14 16:37:25 +0800

    Update

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import Header from './components/Header';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
+import Btn from './components/Btn';
+import Promo from './components/Promo';
+
 import './App.css';
 
 function App() {
@@ -10,6 +13,8 @@ function App() {
       <Header name="Anna" color="purple" />
       <Main greet="Howdy" />
       <Sidebar greet="Hi" />
+      <Btn />
+      <Promo />
     </div>
     )
 }
--- firstapp/src/components/Promo.js
+++ firstapp/src/components/Promo.js
@@ -1,12 +1,18 @@
+// Parent child data flow
+import PromoHeading from "./PromoHeading"
+
+const data = {
+    heading: "Jo mama",
+    callToAction: "Everything must go!"
+}
+
 function Promo() {
     return (
-        <div className = "promo-section">
-            <div>
-                <h1>Don't miss this deal!</h1>
-            </div>
-            <div>
-                <h2>Subscribe to my newsletter and get all the shop items at 50% off!</h2>
-            </div>
+        <div>
+            <PromoHeading 
+            heading = {data.heading}
+            callToAction = {data.callToAction}
+            />
         </div>
     );
 };
