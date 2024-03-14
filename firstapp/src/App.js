6f5c6c66a6acbe5d2fe07d32c7569e9be54e5b8d
Syafiq2112 <azman.syafiq01@gmail.com>
2024-03-14 17:34:55 +0800

    update

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -4,18 +4,32 @@ import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 import Btn from './components/Btn';
 import Promo from './components/Promo';
+import Heading from './components/Heading';
 
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  // Hook: sort of like setter getter 
+  const [word, setWord] = React.useState('Eat')
+
+  function handleClick() {
+    setWord('Drink')
+  }
+
   return (
-    <div>
+    <div className="App">
       <Header name="Anna" color="purple" />
       <Main greet="Howdy" />
       <Sidebar greet="Hi" />
       <Btn />
       <Promo />
+
+      <Heading message={word + " at Little Lemon"}/>
+      <button onClick={handleClick}>Click Here</button>
+
     </div>
+
     )
 }
 
